166f41bd72bd13306d2ab3f592882865a265b4d0
Nick Hardiman <nhardima@redhat.com>
2026-02-25 07:31:46 +0000

    argh not saved

--- libvirt-utils/lab-proxy.sh
+++ libvirt-utils/lab-proxy.sh
@@ -1,3 +1,3 @@
-export   http_proxy=http://gateway.silvan.uk:3128
-export  HTTPS_PROXY=http://gateway.silvan.uk:3128
+export   http_proxy=http://gateway.site1.example.com:3128
+export  HTTPS_PROXY=http://gateway.site1.example.com:3128
 export   no_proxy=localhost,127.0.0.1,example.com
