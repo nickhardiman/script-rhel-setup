ae714268a8b8bde74a629a08a4dd105f86973d7a
Nick Hardiman <nick.hardiman@gmail.com>
2026-03-10 09:22:23 +0000

    comment, back out

--- libvirt-utils/macos-route-add.sh
+++ libvirt-utils/macos-route-add.sh
@@ -2,6 +2,7 @@
 # to get to the lab network, go via the hypervisor.
 # eg. A route is needed to make this command work.
 #   me@workstation:#  ssh ansible_user@192.168.134.5
+#
 # Network is defined in 
 # https://github.com/nickhardiman/ansible-collection-libvirt/blob/main/roles/libvirt_net_private/defaults/main.yml
 # MacOS is BSD, not Linux, and uses the "route add" command.
@@ -10,6 +11,8 @@
 # or
 #   nmcli connection modify enp1s0 +ipv4.routes "$LAB_NET $HYPERVISOR"
 # Virtualbox NAT uses the host network, so this can also fix guest routing.
+# To back out the change, replace "add" with "delete".
+#   route delete -net  $LAB_NET $HYPERVISOR
 #
 LAB_NET="192.168.134.0/24"
 HYPERVISOR=192.168.1.253
